865611be6e5afcbbf673f13990449ccbaac3b0a6
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-09-07 17:57:26 +0700

    Update vcbdigibank.cpp

--- vcbdigibank.cpp
+++ vcbdigibank.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void myFunction() {
   cout << "I send funding to Vietnam!\n";
   cout << "Add bank account and withdraw or send money to bank local Vietnam!\n";
+   cout << "Bank send sms to number phone or report to them!\n";
   cout << "They only provider bank account and get money at bank local of them!\n";
   
 }
