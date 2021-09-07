138e07c03eb7d7055c023cfc5d27ba08b0829d71
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-09-07 17:41:23 +0700

    Create vcbdigibank.cpp

---
+++ vcbdigibank.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void myFunction() {
+  cout << "I send funding to Vietnam!\n";
+  cout << "Add bank account and withdraw or send money to bank local Vietnam!\n";
+  cout << "They only provider bank account and get money at bank local of them!\n";
+  
+}
+
+int main() {
+  myFunction();
+  return 0;
+}
