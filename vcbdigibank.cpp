e1a5483e43374e02e7a5581d20395aaf76df70e8
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-09-08 12:42:21 +0700

    Update vcbdigibank.cpp

--- vcbdigibank.cpp
+++ vcbdigibank.cpp
@@ -6,6 +6,7 @@ void myFunction() {
   cout << "Add bank account and withdraw or send money to bank local Vietnam!\n";
    cout << "Bank send sms to number phone or report to them!\n";
   cout << "They only provider bank account and get money at bank local of them!\n";
+  cout << "Recipients provide information: identity card at the bank, provide money transaction code, amount received, money source from Paypal!\n";
   
 }
 
